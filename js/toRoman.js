690af56d736403d5bea1aeceae306c9b74d343b4
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 11:16:21 -0500

    work in progress

---
+++ js/toRoman.js
@@ -0,0 +1,13 @@
+// # 1. Write a method TO_ROMAN, TO_ROMAN takes in INPUT_NUMBER (an arabic number)
+// # 2. Create a OUTPUT string, set to ''
+// # 3. Create a ROMAN_NUMERAL_TO_ARABIC_MAP that includes roman numerals as keys, arabic numbers as values
+// # 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and ARABIC_NUMBER
+// # 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
+// # 6. If EVENLY_DIVISIBLE_TIMES >= 1
+//   # 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
+//   # 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
+// # 7. Return OUTPUT
+
+function toRoman(num) {
+    
+}
