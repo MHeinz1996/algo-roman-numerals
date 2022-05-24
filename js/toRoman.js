069c0bd11b23f341844d8a2d607ae6d965152419
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 11:56:56 -0500

    Work in progress

--- js/toRoman.js
+++ js/toRoman.js
@@ -9,5 +9,31 @@
 // # 7. Return OUTPUT
 
 function toRoman(num) {
-    
-}
+    const roman = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+
+    let str = '';
+  
+    for (let i of Object.keys(roman)) {
+      let q = Math.floor(num / roman[i]);
+      num -= q * roman[i];
+      str += i.repeat(q);
+    }
+  
+    return str;
+  }
+
+console.log(toRoman(2000));
