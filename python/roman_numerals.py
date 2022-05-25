1add4251c47e35b91e4bd2606a252ba0985813b4
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-24 20:28:12 -0500

    Completed python algo

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,29 @@
 def to_roman(num):
-    # write your code here!
+    input = num
+    output = []
+
+    roman_numeral_to_arabic_map = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+
+    while(num != 0):
+        for key in roman_numeral_to_arabic_map:
+            if((num/roman_numeral_to_arabic_map[key]) >= 1):
+                output.append(key)
+                num = num - roman_numeral_to_arabic_map[key]
+                break
+
+    print(input, '==', ''.join(output))
+    return ''.join(output)
